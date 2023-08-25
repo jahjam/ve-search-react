d444e44d14c68d622545246ffbbffc1eb09c1441
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-25 13:40:55 +0100

    refactor formatting

--- src/components/ResultsCard/ResultsCard.jsx
+++ src/components/ResultsCard/ResultsCard.jsx
@@ -1,43 +1,43 @@
-import { useEffect, useRef} from 'react';
+import {useEffect, useRef} from 'react';
 import truncateTitle from '../../helpers/truncateTitle';
 
 import * as Styled from './styles';
 
 const ResultCard = props => {
-  const resultBoxWidth = useRef();
+    const resultBoxWidth = useRef();
 
-  const onClickHandler = e => {
-    if (e.key !== 'Enter' && e.nativeEvent.type !== 'click') return;
+    const onClickHandler = e => {
+        if (e.key !== 'Enter' && e.nativeEvent.type !== 'click') return;
 
-    props.getResult(props.id);
-  };
+        props.getResult(props.id);
+    };
 
-  const PADDING_SIZE = 40;
-  useEffect(() => {
-    props.setScrollWidth(
-      props.sliderEl.current.scrollWidth - props.sliderEl.current.offsetWidth,
-      +resultBoxWidth.current.getBoundingClientRect().width + PADDING_SIZE
+    const PADDING_SIZE = 40;
+    useEffect(() => {
+        props.setScrollWidth(
+            props.sliderEl.current.scrollWidth - props.sliderEl.current.offsetWidth,
+            +resultBoxWidth.current.getBoundingClientRect().width + PADDING_SIZE
+        );
+    }, [props]);
+
+    return (
+        <Styled.ResultsBox
+            onClick={onClickHandler}
+            animate={{opacity: 1}}
+            ref={resultBoxWidth}
+            tabIndex="0"
+            onKeyDown={onClickHandler}
+        >
+            <Styled.Result>
+                <Styled.ImgContainer>
+                    <img src={`${props.image}`} alt="food"/>
+                </Styled.ImgContainer>
+                <span>{truncateTitle(props.title)}</span>
+                <span>{props.author}</span>
+                <span>{`${Math.floor(props.ratingsAverage)}/5`}</span>
+            </Styled.Result>
+        </Styled.ResultsBox>
     );
-  }, [props]);
-
-  return (
-    <Styled.ResultsBox
-      onClick={onClickHandler}
-      animate={{ opacity: 1 }}
-      ref={resultBoxWidth}
-      tabIndex="0"
-      onKeyDown={onClickHandler}
-    >
-      <Styled.Result>
-        <Styled.ImgContainer>
-          <img src={`${props.image}`} alt="food" />
-        </Styled.ImgContainer>
-        <span>{truncateTitle(props.title)}</span>
-        <span>{props.author}</span>
-        <span>{`${Math.floor(props.ratingsAverage)}/5`}</span>
-      </Styled.Result>
-    </Styled.ResultsBox>
-  );
 };
 
 export default ResultCard;
